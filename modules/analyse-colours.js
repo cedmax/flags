81ba7a8ff857df4a53b49170433459edae58aecc
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-13 20:21:20 +0000

    const

--- modules/analyse-colours.js
+++ modules/analyse-colours.js
@@ -3,8 +3,8 @@ const helpers = require("./helpers");
 const parse = require("pixelbank");
 const { createCanvas, loadImage } = require("canvas");
 
-var rgbToHex = function(rgb) {
-  var hex = Number(rgb).toString(16);
+const rgbToHex = function(rgb) {
+  let hex = Number(rgb).toString(16);
   if (hex.length < 2) {
     hex = "0" + hex;
   }
