e6ebfcda7890bc15d8531b32ef073011d51f77cd
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-19 07:46:31 +0000

    removed background and border from the modal

--- src/App.js
+++ src/App.js
@@ -451,6 +451,8 @@ class App extends Component {
               },
               content: {
                 ...getSize(this.state.detail.ratio),
+                background: "transparent",
+                border: "0",
                 padding: 0,
                 top: "50%",
                 left: "50%",
