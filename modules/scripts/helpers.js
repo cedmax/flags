a584d60e0e173df6547ce77ca4bfcadab97573bf
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-24 00:01:28 +0000

    further refactoring

--- modules/scripts/helpers.js
+++ modules/scripts/helpers.js
@@ -2,11 +2,9 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const axios = require("axios");
 const async = require("async");
-
 const path = process.cwd();
 
 const getTempFile = key => `${path}/modules/.cache/${key}.json`;
-
 const resolveCache = key => {
   const tempFile = getTempFile(key);
   if (fs.existsSync(tempFile)) {
@@ -26,12 +24,79 @@ const merge = (flags, newInfo) =>
         ...newInfo[flag.id],
       }));
 
+const generateHTML = (html, data) => {
+  const $ = cheerio.load(html);
+  const currentDataString = $("#data").html();
+  if (currentDataString) {
+    const currentData = JSON.parse(currentDataString);
+    data = {
+      ...currentData,
+      ...data,
+    };
+  }
+  $("#data").text(JSON.stringify(data));
+
+  return {
+    html: $.html(),
+    data,
+  };
+};
+
+const consolidate = group => flags =>
+  new Promise(resolve => {
+    const dataKey = group || "world";
+    let data = { [dataKey]: flags };
+    const html = fs.readFileSync(`${path}/public/index.html`);
+    const { data: newData, html: newHTML } = generateHTML(html, data);
+
+    fs.writeFileSync(
+      `${path}/src/data/flags.json`,
+      JSON.stringify(newData, null, 4),
+      "UTF-8"
+    );
+    fs.writeFileSync(`${path}/public/index.html`, newHTML, "UTF-8");
+    resolve();
+  });
+
+const required = reject => (obj, properties) => {
+  properties.forEach(prop => {
+    if (!obj[prop]) {
+      reject(`${obj.id} doesn't have ${prop}`);
+    }
+  });
+};
+
+const validate = group => flags =>
+  new Promise((resolve, reject) => {
+    const validatePresent = required(reject);
+
+    flags.forEach(flag => {
+      validatePresent(flag, ["ratio", "colors", "tags"]);
+
+      flag.colors.forEach(color => {
+        validatePresent(color, ["tag"]);
+      });
+
+      if (!group) {
+        validatePresent(flag, ["continents", "adoption", "anthem"]);
+        if (flag.anthem.input.match(/[0-9]{4}/)) {
+          reject(`${flag.id} has the wrong anthem`);
+        }
+        delete flag.anthem.input;
+      }
+    });
+    resolve(flags);
+  });
+
 module.exports = {
+  validate,
+  resolveCache,
+  saveCache,
+  consolidate,
   mergeData: (dataFetcher, cacheKey, group) => flags =>
     new Promise(resolve => {
-      if (group) {
-        cacheKey = `${cacheKey}-${group}`;
-      }
+      cacheKey = cacheKey && group ? `${cacheKey}-${group}` : cacheKey;
+
       if (cacheKey) {
         const content = resolveCache(cacheKey);
         if (content) {
@@ -48,73 +113,6 @@ module.exports = {
 
         return resolve(merge(flags, results));
       });
-    }),
-  resolveCache,
-  saveCache,
-  consolidate: group => flags =>
-    new Promise(resolve => {
-      const dataKey = group || "world";
-      let data = { [dataKey]: flags };
-      const html = fs.readFileSync(`${path}/public/index.html`);
-      const $ = cheerio.load(html);
-      const currentDataString = $("#data").html();
-
-      if (currentDataString) {
-        const currentData = JSON.parse(currentDataString);
-        data = {
-          ...currentData,
-          ...data,
-        };
-      }
-      $("#data").text(JSON.stringify(data));
-      const newHTML = $.html();
-      fs.writeFileSync(
-        `${path}/src/data/flags.json`,
-        JSON.stringify(data, null, 4),
-        "UTF-8"
-      );
-      fs.writeFileSync(`${path}/public/index.html`, newHTML, "UTF-8");
-      resolve();
-    }),
-  validate: group => flags =>
-    new Promise(resolve => {
-      flags.forEach(flag => {
-        if (!flag.ratio) {
-          throw new Error(`${flag.id} doesn't have ratio`);
-        }
-
-        if (!flag.colors) {
-          throw new Error(`${flag.id} doesn't have colors`);
-        } else {
-          flag.colors.forEach(color => {
-            if (!color.tag) {
-              throw new Error(`${flag.id} doesn't have color tags`);
-            }
-          });
-        }
-
-        if (!flag.tags) {
-          throw new Error(`${flag.id} doesn't have tags`);
-        }
-
-        if (!group) {
-          if (!flag.continents) {
-            throw new Error(`${flag.id} doesn't have continents`);
-          }
-          if (!flag.adoption) {
-            throw new Error(`${flag.id} doesn't have adoption`);
-          }
-          if (!flag.anthem) {
-            throw new Error(`${flag.id} doesn't have an anthem`);
-          } else {
-            if (flag.anthem.input.match(/[0-9]{4}/)) {
-              throw new Error(`${flag.id} has the wrong anthem anthem`);
-            }
-            delete flag.anthem.input;
-          }
-        }
-      });
-      resolve(flags);
     }),
 
   saveFlagFiles: (flags, callback) =>
