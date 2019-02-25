18f19592b65857c78790ce53e2e0d7590a66bc63
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-25 20:22:25 +0000

    missing label and style fixes

--- src/components/NavSearch.js
+++ src/components/NavSearch.js
@@ -4,12 +4,15 @@ import { withContext } from "../store/context";
 
 const NavSearch = React.memo(({ query, dispatch }) => (
   <div className="filters">
-    <span>Filter: </span>{" "}
-    <input
-      type="text"
-      value={query}
-      onChange={e => dispatch(action("filterString", e.target.value))}
-    />
+    <label for="search">
+      <span>Filter: </span>{" "}
+      <input
+        id="search"
+        type="text"
+        value={query}
+        onChange={e => dispatch(action("filterString", e.target.value))}
+      />
+    </label>
   </div>
 ));
 
