10e1c12b77a4cf03425cea748ff9d291b510fd77
Marco Cedaro <cedmax@users.noreply.github.com>
2019-02-28 20:05:13 +0000

    lazyload placeholder

--- src/components/ListItemFront.js
+++ src/components/ListItemFront.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default React.memo(({ svgUrl, country }) => (
   <div className="front">
     <figure>
-      <LazyLoadImage width={180} src={svgUrl} alt={`Flag of ${country}`} />
+      <LazyLoadImage
+        placeholder={<div className="spinner spinner-small" />}
+        width={180}
+        src={svgUrl}
+        alt={`Flag of ${country}`}
+      />
       <figcaption>{country}</figcaption>
     </figure>
   </div>
