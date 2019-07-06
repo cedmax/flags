d9f76a978ff0d960b61bda18509399f753fe23be
Marco Cedaro <cedmax@users.noreply.github.com>
2019-07-06 21:35:42 +0100

    setup netlify redirects to access country flags by isocode

---
+++ modules/set-redirects.js
@@ -0,0 +1,15 @@
+const assets = require("../build/asset-manifest.json");
+const isocodes = require("./iso.json");
+const fs = require("fs");
+
+let redirects = "";
+const countryIds = Object.keys(isocodes);
+countryIds.forEach(id => {
+  const assetKey = `static/media/${id}.svg`;
+  const asset = assets[assetKey];
+  redirects += `\nhttps://flags.dsgn.it/assets/${isocodes[
+    id
+  ].toLowerCase()}.svg ${asset} 200`;
+});
+
+fs.appendFileSync("./build/_redirects", redirects, "utf-8");
