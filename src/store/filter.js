cc02e646c8861d01ff80c4fb6262b36d4dbdc786
Marco Cedaro <cedmax@users.noreply.github.com>
2022-05-19 23:01:22 +0100

    enhance search

--- src/store/filter.js
+++ src/store/filter.js
@@ -1,17 +1,22 @@
 exports.byColour = (filtered, filters) =>
-  filtered.filter(({ tags }) => filters.every(tag => tags.includes(tag)));
+  filtered.filter(({ tags }) => filters.every((tag) => tags.includes(tag)));
 
 exports.byContinent = (filtered, continent) =>
-  filtered.filter(flag => flag.continents.includes(continent));
+  filtered.filter((flag) => flag.continents.includes(continent));
 
 exports.bySearch = (filtered, q) => {
   const qF = q.toLowerCase();
-  const matches = ["(", " "].map(sign => sign + qF);
+  const matches = ["(", " "].map((sign) => sign + qF);
 
-  return filtered.filter(flag => {
+  return filtered.filter((flag) => {
     const country = flag.country.toLowerCase();
+    const belongsTo = (flag.belongsTo && flag.belongsTo.toLowerCase()) || "";
     return (
-      country.startsWith(qF) || matches.some(match => country.includes(match))
+      country.startsWith(qF) ||
+      matches.some(
+        (match) => country.includes(match) || belongsTo.includes(match)
+      ) ||
+      belongsTo.startsWith(qF)
     );
   });
 };
